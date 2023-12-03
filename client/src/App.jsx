6431a9c26020695854da2a857f6bbb6d3e35fb31
Ivanina <iwanina@gmail.com>
2023-12-03 11:37:54 +0200

    Add Home component and import react Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header.jsx"
+import Home from "./components/Home/Home.jsx"
 function App() {
 
   return (
     <div id="box">
       <Header />
+      <Home />
     </div>
   )
 }
