6baff38df887bcb291b29e5e618757713bd98eb8
Ivanina <iwanina@gmail.com>
2023-12-04 10:52:35 +0200

    add navigation CreateGame

--- client/src/components/GameCreate/GameCreate.jsx
+++ client/src/components/GameCreate/GameCreate.jsx
@@ -1,9 +1,22 @@
+import * as gameService  from '../../services/gameService.js'
+import { useNavigate } from 'react-router-dom';
+
 export default function GameCreate () {
-    const createGameSubmitHandler = (e) => {
+    const navigate = useNavigate();
+
+    const createGameSubmitHandler = async (e) => {
         e.preventDefault();
 
         const gameData = Object.fromEntries(new FormData(e.currentTarget));
         console.log(gameData);
+        try {
+            await gameService.create(gameData);
+            navigate('/games')
+        } catch (error) {
+            //Error notification
+            console.log(error)
+        }
+        
     }
 
     return (
