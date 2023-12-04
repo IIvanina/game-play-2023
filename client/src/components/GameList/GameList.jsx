3096f6c5f1872d72b42bb38661b1ceb6597c7cef
Ivanina <iwanina@gmail.com>
2023-12-04 20:52:19 +0200

    Show dynamic gameList

--- client/src/components/GameList/GameList.jsx
+++ client/src/components/GameList/GameList.jsx
@@ -1,45 +1,25 @@
 import { useEffect, useState } from "react";
 import * as gameService from '../../services/gameService.js'
+import GameListItem from "./GameListItem/GameListItem.jsx";
 
 export default function GameList () {
-    const {games, setGames} = useState([]);
+    const [games, setGames] = useState([]);
     useEffect(() => {
         gameService.getAll()
             .then(result => setGames(result))
+            console.log(games)
     }, [])
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
+
+            {games.map(game => (
+                <GameListItem key={games._id} {...game} />
+            ))}
+
+            {games.length === 0 && <h3 className="no-articles">No articles yet</h3>}
             
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
             
-            <h3 className="no-articles">No articles yet</h3>
         </section>
     );
 }
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -3,12 +3,12 @@ import { request } from '../lib/request.js'
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-   await request('GET', baseUrl) 
+   const result = await request('GET', baseUrl) 
    return Object.values(result)
 };
 
 export const create = async (gameData) => {
-    const response = await fetch(`${baseUrl}`, {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
